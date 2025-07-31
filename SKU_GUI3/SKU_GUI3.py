f852b096a8c3d55a8a561538d8d45b6143ff2c04
Arnau <zimzf23@gmail.com>
2025-07-31 14:25:56 +0200

    passat a clases, declarada a global a partir de toml

--- SKU_GUI3/SKU_GUI3.py
+++ SKU_GUI3/SKU_GUI3.py
@@ -2,6 +2,7 @@
 #Import Modules
 from dependencies import *
 from ui_components import header_and_search, main_layout
+from Catalog import build_section_classes
 
 @ui.page('/home')
 def page_layout():
@@ -16,9 +17,25 @@ def page_layout():
 def page_layout():
     # Build Header
     header_and_search(None)
-
+    ui.button('Create New Item', on_click=lambda: test_config()).props('color=primary')
     # Build Body
     main_layout()
+    
+
+def test_config():
+    # Test if config file is loaded correctly
+    sections, FullItem = build_section_classes("data.toml")
+
+    # instantiate one item
+    item = FullItem()
+    print(item)  # you get nested dataclasses like item.basic, item.decoded, etc.
+
+    # access and mutate
+    item.basic.ref = "REF001"
+    item.basic.name = 'Hello!'
+    item.mechanical.weight = 42.0
+    print(item)  # you get nested dataclasses like item.basic, item.decoded, etc.
+
 
 # Navigate to the home page when the application starts
 ui.navigate.to('/home')
